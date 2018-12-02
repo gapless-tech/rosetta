cfa367408e67c45bdca85599629788f38f453951
Matheus Paiva <matheus.paiva@hexis-hub.com>
2018-12-02 23:32:07 +0000

    Update webpack config to be able to be imported like a package

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,9 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    library: '@gapless/rosetta',
+    libraryTarget: 'commonjs2',
+  },
 };
